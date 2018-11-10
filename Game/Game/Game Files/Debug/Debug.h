d22fafbcc2ed05d61b77216b0e2393207c07ce49
Gabriel Beaudet <gab950912@hotmail.com>
2018-11-10 12:59:47 -0500

    Fix debug

--- Game/Game/Game Files/Debug/Debug.h
+++ Game/Game/Game Files/Debug/Debug.h
@@ -24,3 +24,28 @@
 
 
 #endif // __DEBUG
+#pragma once
+
+#include<Windows.h>
+#include<tchar.h>
+#include <string>
+
+
+#define STRINGIZE(x) STRINGIZE_EXPAND(x)
+#define STRINGIZE_EXPAND(x) #x
+#define LINE_STRING STRINGIZE(__LINE__) //Line number converted to string for Message box
+
+#define sDelete(var) do{delete var; var = nullptr;} while(0)
+
+#ifdef _DEBUG
+
+
+#define sNew new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#define ERR_MSG(str) do{OutPutDebugString(_T( str "\n\nFile: " __FILE__ "\nLine: " LINE_STRING "\n")); MessageBox(NULL, _T( str "\n\nFile: " __FILE__ "\nLine: " LINE_STRING "\n"), _T("ERROR"), NULL);} while(0)
+
+#else
+
+#define sNew new
+#define ERR_MSG(str) do{ MessageBox(NULL, _T( str "\n\nFile: " __FILE__ "\nLine: " LINE_STRING "\n" ), _T("ERROR"), NULL);} while(0)
+
+#endif // __DEBUG
